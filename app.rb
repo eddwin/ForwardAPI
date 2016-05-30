4392fbdeea0590e72d9a127e0175fa11dc91a1e3
Edwin Mejia <eddwin@live.com>
2016-05-31 02:23:58 +0800

    final changes?

--- app.rb
+++ app.rb
@@ -13,17 +13,20 @@ class PowerAPI < Sinatra::Base
 
   post '/loads' do
     content_type :json
-    body = request.body.read
-    logger.info body
-    time = Time.now
     begin
-      req = JSON.parse(body)
+      req = JSON.parse(request.body.read)
       logger.info req
-    rescue Exception => e
-      puts e.message
+    rescue
       halt 400
     end
-    req
+    load = Load.new
+    load.loads = req['loads'].to_json
+    load.time = Time.now.t­o_f
+
+    if load.save
+      status 201
+    end
+    
   end
 
 
